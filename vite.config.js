1e35bebf5f0608aec83409db0b0e3ba1a45060e2
joaooliveira <joao.oliveira0117@hotmail.com>
2024-08-09 00:45:38 -0300

    update vite manifest

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,15 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
                 'resources/js/app.js',
+                'resources/js/createEventPage.js',
+                'resources/js/eventsPage.js',
+                'resources/js/htmlBuilder.js',
                 'resources/js/loginPage.js',
+                'resources/js/registerPage.js',
+                'resources/js/ticketsPage.js',
+                'resources/js/updateEventPage.js',
+                'resources/js/userComponent.js',
             ],
             refresh: true,
         }),
